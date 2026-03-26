8f8fe7b14de36e0caa7ad7a246c032999261d5d2
Diego <dasc2703@gmail.com>
2026-03-26 13:01:29 -0500

    Archivos no usados eliminados

--- src/util/buildPokemonQuery.js
+++
@@ -1,5 +0,0 @@
-export const buildPokemonQuery = (field, query ) => {
-    return query.trim().split(/\s+/).map((q, index) => (
-        index === 0 ? `${field}:*${q}*` : `AND ${field}:*${q}*`
-    )).join(' ')
-}
